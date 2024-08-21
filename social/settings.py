53df0d93dd6ff2a10e18468e757d5f244b73dcb2
Sagnik Chakraborty <sagnify2022@gmail.com>
2024-08-22 00:12:04 +0530

    db changes

--- social/settings.py
+++ social/settings.py
@@ -83,12 +83,24 @@ WSGI_APPLICATION = 'social.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'LrXCLSbGeElATNCuRBWbzGhlVlmfNiwN',
+        'HOST': 'postgres.railway.internal',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
